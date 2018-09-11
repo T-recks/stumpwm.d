997a7bc0595308b90cd90054b73e693034defe0b
tjr <tjr@airmail.cc>
2018-09-11 00:42:03 -0700

    use MESSAGE

--- swank-setup.lisp
+++ swank-setup.lisp
@@ -3,38 +3,35 @@
 (load "~/.emacs.d/elpa/slime-20180820.2223/swank-loader.lisp")
 (swank-loader:init)
 
-(defun echo-screen (str)
-  (echo-string (current-screen) str))
-
-(defcommand start-swank () ()
+(defcommand swank-start () ()
   (if (and (boundp '*server-running*) *server-running*)
       (echo-screen "Swank server already running.")
       (progn
         (swank:create-server :port 4004)
-        (echo-screen "Starting swank on localhost:4004.")
+        (message "Starting swank on localhost:4004.")
         (defparameter *server-running* t))))
 
-(defcommand stop-swank () ()
+(defcommand swank-stop () ()
   (if (and (boundp '*server-running*) *server-running*)
       (progn
-        (echo-screen "Stopping swank on localhost:4004.")
+        (message "Stopping swank on localhost:4004.")
         (swank:stop-server 4004)
         (defparameter *server-running* nil))
-      (echo-screen "No server running.")))
+      (message "No server running.")))
 
-(defcommand toggle-swank () ()
+(defcommand swank-toggle () ()
   (if *server-running*
       (stop-swank)
       (start-swank)))
 
-(defcommand restart-swank () ()
+(defcommand swank-restart () ()
   (stop-swank)
   (start-swank))
 
-(defcommand status-swank () ()
+(defcommand swank-status () ()
   (if *server-running*
-      (echo-screen "Swank is currently running on localhost:4004.")
-      (echo-screen "Swank is not currently running.")))
+      (message "Swank is currently running on localhost:4004.")
+      (message "Swank is not currently running.")))
 
 (unless (boundp '*server-running*)
   (start-swank))
