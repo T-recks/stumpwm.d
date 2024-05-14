3ca9232363683b06ae1ddddd734ba8807fa5f3b0
T-recks <timjrecker@gmail.com>
2024-05-14 14:43:57 -0400

    use system :swank

--- swank-setup.lisp
+++ swank-setup.lisp
@@ -1,16 +1,18 @@
 (in-package :stumpwm-user)
 
-(load "~/.emacs.d/elpa/slime-20200530.1629/swank-loader.lisp")
+(require 'swank)
 
-(swank-loader:init)
+(defun swank-init ()
+  (swank-loader:init)
+  )
 
 (defcommand swank-start () ()
-  (if (and (boundp '*server-running*) *server-running*)
-      (message "Swank server already running.")
-      (progn
-        (swank:create-server :port 4004 :dont-close t)
-        (message "Starting swank on localhost:4004.")
-        (defparameter *server-running* t))))
+   (if (and (boundp '*server-running*) *server-running*)
+       (message "Swank server already running.")
+       (progn
+         (swank:create-server :port 4004 :dont-close t)
+         (message "Starting swank on localhost:4004.")
+         (defparameter *server-running* t))))
 
 (defcommand swank-stop () ()
   (if (and (boundp '*server-running*) *server-running*)
